cd15b0687d6e19ff2884a874c5d4f0775840e258
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-28 21:54:16 -0600

    connected functions to buttons.  Need to add the real functionality to connect server and client

--- public/javascripts/http-stuff.js
+++ public/javascripts/http-stuff.js
@@ -2,77 +2,87 @@
 
 (function () {
 
-    function getStuff() { // The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
-
+    function getStuff() {
+        console.log("The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.");
     }
 
-    function headStuff() { // The HEAD method asks for a response identical to a GET request, but without the response body.
-
+    function headStuff() {
+        console.log("The HEAD method asks for a response identical to a GET request, but without the response body.")
     }
 
-    function postStuff() { // The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.
-
+    function postStuff() {
+        console.log(" The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.")
     }
 
-    function putStuff() { // The PUT method replaces all current representations of the target resource with the request payload.
-
+    function putStuff() {
+        console.log("The PUT method replaces all current representations of the target resource with the request payload.")
     }
 
-    function deleteStuff() { // The DELETE method deletes the specified resource.
-
+    function deleteStuff() {
+        console.log("The DELETE method deletes the specified resource.")
     }
 
-    function connectStuff() { // The CONNECT method establishes a tunnel to the server identified by the target resource.
-
+    function connectStuff() {
+        console.log("The CONNECT method establishes a tunnel to the server identified by the target resource.")
     }
 
-    function optionsStuff() { // The OPTIONS method describes the communication options for the target resource.
-
+    function optionsStuff() {
+        console.log("The OPTIONS method describes the communication options for the target resource.")
     }
 
-    function traceStuff() { // The TRACE method performs a message loop-back test along the path to the target resource.
-
+    function traceStuff() {
+        console.log("The TRACE method performs a message loop-back test along the path to the target resource.")
     }
 
-    function patchStuff() { // The PATCH method applies partial modifications to a resource.
-
+    function patchStuff() {
+        console.log("The PATCH method applies partial modifications to a resource.")
     }
+
     let displayArea = document.querySelector("#display-area");
     let getButton = document.querySelector("#get-button");
     getButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Get' button";
+        displayArea.innerHTML = "You clicked the 'Get' button.  Check console for description.";
+        getStuff();
     })
     let headButton = document.querySelector("#head-button");
     headButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Head' button";
+        displayArea.innerHTML = "You clicked the 'Head' button.  Check console for description.";
+        headStuff();
     })
     let postButton = document.querySelector("#post-button");
     postButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Post' button";
+        displayArea.innerHTML = "You clicked the 'Post' button.  Check console for description.";
+        postStuff();
     })
     let putButton = document.querySelector("#put-button");
     putButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Put' button";
+        displayArea.innerHTML = "You clicked the 'Put' button.  Check console for description.";
+        putStuff();
     })
     let deleteButton = document.querySelector("#delete-button");
     deleteButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Delete' button";
+        displayArea.innerHTML = "You clicked the 'Delete' button.  Check console for description.";
+        deleteStuff();
     })
     let connectButton = document.querySelector("#connect-button");
     connectButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Connect' button";
+        displayArea.innerHTML = "You clicked the 'Connect' button.  Check console for description.";
+        connectStuff();
     })
     let optionsButton = document.querySelector("#options-button");
     optionsButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Options' button";
+        displayArea.innerHTML = "You clicked the 'Options' button.  Check console for description.";
+        optionsStuff();
     })
     let traceButton = document.querySelector("#trace-button");
     traceButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Trace' button";
+        displayArea.innerHTML = "You clicked the 'Trace' button.  Check console for description.";
+        traceStuff();
     })
     let patchButton = document.querySelector("#patch-button");
     patchButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Patch' button";
+        displayArea.innerHTML = "You clicked the 'Patch' button.  Check console for description.";
+        patchStuff();
     })
 
 }());
