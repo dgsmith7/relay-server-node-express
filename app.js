e40e479bf897e3d2ba717ecab36d8400273fb507
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-12-04 10:00:01 -0600

    cleaned up. will use an API call to finish demo next

--- app.js
+++ app.js
@@ -9,21 +9,21 @@
 
     app.use(bodyParser.json());
 
-    const someData = {quote: "Everything good is wild and free."};
+    // Here we are serving up http pages with GET
     app.use(express.static(path.join(__dirname, 'public')));
 
-    // app.get('/', function (req, res) {
-    //         console.log("Yes - in the server for page.")
-    //         res.sendFile(path.join(__dirname,'/index.html'));
-    // });
-    app.get('/', (req, res) => res.send(someData));
+    // Some JSON object to be sent back to a request
+    const someData = {quote: "Everything good is wild and free."};
+
+    // This reacts to POST requests.  And here I am doing different things based on the
+    // body of the post request sent.  This will allow me to, for example, make an API
+    // call from the backend using environment variables to avoid exposing API keys to
+    // the front end.
     app.post('/', (req, res) => {
         console.log("The req body as per server", req.body);
         if (req.body.info === 'data') {
-            console.log("yes data");
             res.send(JSON.stringify(someData));
         } else {
-            console.log('no data');
             res.send(JSON.stringify({quote: 'This will call the API behind the scenes and you can do different things based on body of the request.'}));
         }
     });
@@ -32,41 +32,5 @@
         console.log(`Listening on port ${port}!`);
     });
 
-
-    // app.get('/', (req, res) => {
-    //     res.send('Hello World!')
-    // })
-    //
-    // app.post('/', (req, res) => {
-    //     res.send('Got a POST request')
-    // })
-    //
-    // app.put('/user', (req, res) => {
-    //     res.send('Got a PUT request at /user')
-    // })
-    //
-    // app.delete('/user', (req, res) => {
-    //     res.send('Got a DELETE request at /user')
-    // })
-    //
-//    app.use(express.static(path.join(__dirname, 'public')))
-
-    // catch 404 and forward to error handler
-    // app.use(function (req, res, next) {
-    //     next(createError(404));
-    // });
-
-// error handler
-//     app.use(function (err, req, res, next) {
-//         // set locals, only providing error in development
-//         res.locals.message = err.message;
-//         res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//         // render the error page
-//         res.status(err.status || 500);
-//         res.render('error');
-//     });
-
     module.exports = app;
-
 }());
--- public/javascripts/http-stuff.js
+++ public/javascripts/http-stuff.js
@@ -3,32 +3,22 @@
 (function () {
 
     function getStuff() {
-        console.log("The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.");
-        const url = 'http://localhost:8080';
-        const options = {
-            method: 'GET'
-        };
-        return fetch(url, options)
-            .then((response) => response.json())
-            .then((data) => {
-                displayArea.innerHTML += "\nGET returned " + data.quote;
-            })
-            .catch((e) => console.log("There was an error with the GET request:\n" + e));
+        displayArea.innerHTML += "The GET method requests a representation of the specified resource. Requests using GET should only retrieve data. In this situation, we are using GET to fetch the HTML page and not sending a GET request with FETCH.";
     }
 
     function headStuff() {
-        console.log("The HEAD method asks for a response identical to a GET request, but without the response body.")
+        displayArea.innerHTML += "The HEAD method asks for a response identical to a GET request, but without the response body. ";
     }
 
     function postStuff() {
-        console.log(" The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.")
+        displayArea.innerHTML += " The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server. And here I can do different things based on the body of the post request sent.  This will allow me to, for example, make an API call from the backend using environment variables to avoid exposing API keys to the front end.  Try altering the body of this post request in the code.";
         const url = 'http://localhost:8080';
         const options = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({info: 'data'})
+            body: JSON.stringify({info: 'data'}) // try changing this to something besides 'data'
         };
         return fetch(url, options)
             .then((response) => response.json())
@@ -40,73 +30,73 @@
     }
 
     function putStuff() {
-        console.log("The PUT method replaces all current representations of the target resource with the request payload.")
+        displayArea.innerHTML += "The PUT method replaces all current representations of the target resource with the request payload.";
     }
 
     function deleteStuff() {
-        console.log("The DELETE method deletes the specified resource.")
+        displayArea.innerHTML += "The DELETE method deletes the specified resource.";
     }
 
     function connectStuff() {
-        console.log("The CONNECT method establishes a tunnel to the server identified by the target resource.")
+        displayArea.innerHTML += "The CONNECT method establishes a tunnel to the server identified by the target resource.";
     }
 
     function optionsStuff() {
-        console.log("The OPTIONS method describes the communication options for the target resource.")
+        displayArea.innerHTML += "The OPTIONS method describes the communication options for the target resource.";
     }
 
     function traceStuff() {
-        console.log("The TRACE method performs a message loop-back test along the path to the target resource.")
+        displayArea.innerHTML += "The TRACE method performs a message loop-back test along the path to the target resource.";
     }
 
     function patchStuff() {
-        console.log("The PATCH method applies partial modifications to a resource.")
+        displayArea.innerHTML += "The PATCH method applies partial modifications to a resource.";
     }
 
     let displayArea = document.querySelector("#display-area");
     let getButton = document.querySelector("#get-button");
     getButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Get' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Get' button. ";
         getStuff();
     })
     let headButton = document.querySelector("#head-button");
     headButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Head' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Head' button. ";
         headStuff();
     })
     let postButton = document.querySelector("#post-button");
     postButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Post' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Post' button. ";
         postStuff();
     })
     let putButton = document.querySelector("#put-button");
     putButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Put' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Put' button. ";
         putStuff();
     })
     let deleteButton = document.querySelector("#delete-button");
     deleteButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Delete' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Delete' button. ";
         deleteStuff();
     })
     let connectButton = document.querySelector("#connect-button");
     connectButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Connect' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Connect' button. ";
         connectStuff();
     })
     let optionsButton = document.querySelector("#options-button");
     optionsButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Options' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Options' button. ";
         optionsStuff();
     })
     let traceButton = document.querySelector("#trace-button");
     traceButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Trace' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Trace' button. ";
         traceStuff();
     })
     let patchButton = document.querySelector("#patch-button");
     patchButton.addEventListener("click", () => {
-        displayArea.innerHTML = "You clicked the 'Patch' button.  Check console for description.";
+        displayArea.innerHTML = "You clicked the 'Patch' button. ";
         patchStuff();
     })
 
